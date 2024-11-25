96b9bd686055f6f79ec7001934b40656f66e9536
JTwinleaf <jongarnett1996@gmail.com>
2024-11-25 09:34:21 -0600

    started work on char_report function in main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    list_of_char = get_char_count(text)
+    char_report = get_char_report(list_of_char)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
+    print(list_of_char)
 
 
 def get_num_words(text):
@@ -14,7 +19,17 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_char_count():
+def get_char_count(text):
+    count = {}
+    characters = list(text.lower())
+    for char in characters:
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+    return count
+
+def get_char_report(dict):
     pass
 
 main()
